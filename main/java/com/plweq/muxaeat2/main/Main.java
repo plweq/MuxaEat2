38552232354ec976f130efcf401d892ec47968aa
Ilya <136472058+plweq@users.noreply.github.com>
2025-01-13 19:12:38 +0300

    Add files via upload

---
+++ main/java/com/plweq/muxaeat2/main/Main.java
@@ -0,0 +1,14 @@
+package com.plweq.muxaeat2.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.MODVERSION)
+public class Main {
+
+    public static final String MODID = "muxaeat2";
+    public static final String MODNAME = "MuxaEat 2";
+    public static final String MODVERSION = "1.0.0";
+
+
+
+}
